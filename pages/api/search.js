d70d14b3eb54be3c7acad74f94e1957d2e58c75b
Stefan Djikic <stefandjikic@outlook.com>
2021-12-12 18:43:48 +0100

    Cache posts

--- pages/api/search.js
+++ pages/api/search.js
@@ -5,11 +5,11 @@ import matter from "gray-matter";
 export default function handler(req, res) {
   let posts;
   if (process.env.NODE_ENV === "production") {
-    //TODO - cash files
+    posts = require("../../cache/data").posts;
   } else {
     const files = fs.readdirSync(path.join("posts"));
     posts = files.map((filename) => {
-      const slug = filename.replace('.md', '');
+      const slug = filename.replace(".md", "");
       const markdown = fs.readFileSync(path.join("posts", filename), "utf-8");
       const { data: frontMatter } = matter(markdown);
 
@@ -26,5 +26,5 @@ export default function handler(req, res) {
       category.toLowerCase().indexOf(req.query.value) !== -1
   );
 
-  res.status(200).json(JSON.stringify({results}));
+  res.status(200).json(JSON.stringify({ results }));
 }
